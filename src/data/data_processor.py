2585f40dd9c6139087e80ed06ca32b5f428e0d3b
Marco <marco.prignoli@outlook.it>
2026-07-01 09:45:21 +0200

    Add control for the existence of raw data files and modify comments.

--- src/data/data_processor.py
+++ src/data/data_processor.py
@@ -26,12 +26,12 @@ def _process_data(data: pd.DataFrame) -> pd.DataFrame:
     data["avg_current"] = (data["High"] + data["Low"]) / 2 # create column with avg price of each day
 
     for prev in PREVIOUS_DAYS:
-        # create columns witch the avg prices of the previous 7 days, one for each column
+        # create columns witch the avg prices of the previous days, one for each column
         data[f"avg_prev_{prev}"] = data["avg_current"].shift(prev)
 
-    previous_data = data["avg_current"].shift(1) # Series containing the previous 7 avg prices (current day excluded)
+    previous_data = data["avg_current"].shift(1) # Series containing the previous avg prices (current day excluded)
     for window_size in WINDOW_SIZES:
-        # sliding window of the previouse 7 days
+        # sliding window of the previouse days
         rolling_values = previous_data.rolling(window=window_size) 
 
         # calculate statistics of previous 7 days for each row
@@ -41,7 +41,7 @@ def _process_data(data: pd.DataFrame) -> pd.DataFrame:
         data[f"stat_std_{window_size}"] = rolling_values.std()
 
     for day in TARGET_DAYS:
-        # the model try to predict for each row the avg price for the next 7 days
+        # the model try to predict for each row the avg price for the next days
         data[f"Target_Average_Price_{day}"] = data["avg_current"].shift(-day)
 
     data = data.dropna()
@@ -49,9 +49,8 @@ def _process_data(data: pd.DataFrame) -> pd.DataFrame:
 
     # columns of the processed dataset
     columns = ["Date"] + FEATURE_COLUMNS + TARGET_COLUMNS
-    data = data[columns]
 
-    return data
+    return data[columns]
 
 
 def process_and_save_ticker(ticker: str) -> None:
@@ -63,6 +62,8 @@ def process_and_save_ticker(ticker: str) -> None:
     """
     
     raw_data_path = RAW_DATA_DIR / f"{ticker}.csv"
+    if not raw_data_path.exists():
+        raise FileNotFoundError(f"Raw data file not found: {raw_data_path}")
     data = pd.read_csv(raw_data_path)
 
     processed_data = _process_data(data)
